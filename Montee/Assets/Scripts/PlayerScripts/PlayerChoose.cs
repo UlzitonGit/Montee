397cc7546c78a539e05b12fb04c36f9da5974ecb
UlzitonGit <anton.ulzibat@gmail.com>
2024-10-05 14:56:14 +0300

    Summon

---
+++ Montee/Assets/Scripts/PlayerScripts/PlayerChoose.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerChoose : MonoBehaviour
+{
+    public bool heroActive = true;
+    public bool summonActive = false;
+    public bool isSummonSpawned = false;
+    [SerializeField] PlayerMovement player;
+    public PlayerMovement summon;
+    ThrowSummon thrwSummon;
+    private void Start()
+    {
+        thrwSummon = GetComponentInChildren<ThrowSummon>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            ChangeControl();
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            BackToPlayer();
+        }
+    }
+    public void ChangeControl()
+    {
+        print("h");
+        if (heroActive == true && isSummonSpawned == true)
+        {
+            heroActive = false;
+            summonActive = true;
+            player.enabled = heroActive;
+            summon.enabled = summonActive;
+            player.rb.velocity = new Vector3(0, 0, 0);
+        }
+        else if (summonActive == true && isSummonSpawned == true)
+        {
+            heroActive = true;
+            summonActive = false;
+            player.enabled = heroActive;
+            summon.enabled = summonActive;
+            summon.rb.velocity = new Vector3(0, 0, 0);
+        }
+    }
+    private void BackToPlayer()
+    {
+        isSummonSpawned = false;
+        heroActive = true;
+        summonActive = false;
+        player.enabled = heroActive;
+        summon.enabled = summonActive;
+        Destroy(summon.gameObject);
+        thrwSummon.isSpawned = false;
+    }
+}
--- Montee/Assets/Scripts/SummonScript/SummonBullet.cs
+++ Montee/Assets/Scripts/SummonScript/SummonBullet.cs
@@ -7,6 +7,9 @@ public class SummonBullet : MonoBehaviour
     private float bulletSpeed = 20f;
     private Rigidbody2D rbBullet;
     [SerializeField] Vector3 dir;
+    [SerializeField] LayerMask groundLayer;
+    [SerializeField] GameObject toSummon;
+    bool hited = false;
     void Start()
     {
         
@@ -14,6 +17,22 @@ public class SummonBullet : MonoBehaviour
 
     void Update()
     {
+        CheckGround();
         transform.Translate(dir * bulletSpeed * Time.deltaTime);
+        if (hited == true)
+        {
+            GameObject summon = Instantiate(toSummon,transform.position, Quaternion.identity);
+            PlayerChoose pl = FindFirstObjectByType<PlayerChoose>();
+            pl.isSummonSpawned = true;         
+            pl.summon = summon.GetComponent<PlayerMovement>();
+            pl.ChangeControl();
+            Destroy(gameObject);
+        }
+    }
+    private void CheckGround()
+    {
+        Collider2D[] collider = Physics2D.OverlapCircleAll(transform.position, 0.2f, groundLayer);
+        hited = collider.Length > 0;
+
     }
 }
