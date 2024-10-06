4a5020bb97ac38eca854dbc7f6a7623e652c9af1
UlzitonGit <anton.ulzibat@gmail.com>
2024-10-06 16:59:49 +0300

    Fixed vol 2

--- Montee/Assets/Scripts/Quests/PlateScript.cs
+++ Montee/Assets/Scripts/Quests/PlateScript.cs
@@ -9,6 +9,7 @@ public class PlateScript : MonoBehaviour
     [SerializeField] GameObject door;
     [SerializeField] Transform target;
     [SerializeField] Transform target2;
+    [SerializeField] int speed = 3;
     public bool onPressed = false;
     Transform pos;
     void Start()
@@ -19,8 +20,8 @@ public class PlateScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(onPressed == true) door.transform.position = Vector2.MoveTowards(door.transform.position, new Vector2(target.position.x, target.position.y), 3 * Time.deltaTime);
-        if(onPressed == false) door.transform.position = Vector2.MoveTowards(door.transform.position, new Vector2(target2.position.x, target2.position.y), 3 * Time.deltaTime);
+        if(onPressed == true) door.transform.position = Vector2.MoveTowards(door.transform.position, new Vector2(target.position.x, target.position.y), speed * Time.deltaTime);
+        if(onPressed == false) door.transform.position = Vector2.MoveTowards(door.transform.position, new Vector2(target2.position.x, target2.position.y), speed * Time.deltaTime);
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
