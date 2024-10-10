fcc72e9b3cffea78c4a31031da7756cdd5319bec
UlzitonGit <anton.ulzibat@gmail.com>
2024-10-10 18:20:45 +0300

    vfx

--- Montee/Assets/Scripts/PlayerScripts/PlayerHealth.cs
+++ Montee/Assets/Scripts/PlayerScripts/PlayerHealth.cs
@@ -1,5 +1,6 @@
 
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
@@ -8,6 +9,8 @@ public class PlayerHealth : MonoBehaviour
     [SerializeField] Image hpBar;
     private float maxHealth = 100;
     public float currentHealth;
+    [SerializeField] Volume volume;
+    [SerializeField] Animator anim;
     
     void Start()
     {
@@ -20,11 +23,17 @@ public class PlayerHealth : MonoBehaviour
         currentHealth -= damage;
         if(currentHealth > maxHealth)  currentHealth = maxHealth; 
         print(currentHealth / maxHealth);
+       
+        if(damage > 0) anim.SetTrigger("damage");
         hpBar.fillAmount = currentHealth / maxHealth;
         if(currentHealth <= 0)
         { 
             int scene = SceneManager.GetActiveScene().buildIndex;
             SceneManager.LoadScene(scene);
+        }
+        else
+        {
+            volume.weight = 1 - (currentHealth / maxHealth);
         }
     }
 }
